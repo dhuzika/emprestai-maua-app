21ace4811d0cc36257f6f3aede870367342f37db
dhuzika <dhuzika@outlook.com>
2023-05-18 19:21:43 -0300

    Update login.dart
    responsividade no login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -24,76 +24,82 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Center(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(top: 0, bottom: 120.0),
-                child: Text(
-                  'Emprestaí,\n    Mauá!',
-                  style: TextStyle(fontSize: _getFontSize(context)),
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(top: 0, bottom: 120.0),
+                      child: Text(
+                        'Emprestaí,\n    Mauá!',
+                        style: TextStyle(fontSize: _getFontSize(context)),
+                      ),
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return 'Por favor, informe o login';
+                        }
+                        return null;
+                      },
+                      textInputAction: TextInputAction.next,
+                      onSaved: (value) => _username = value,
+                      style: TextStyle(fontSize: _getFontSize(context)),
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(left: 16.0),
+                          labelText: 'Login',
+                          labelStyle: TextStyle(fontSize: 20.0)),
+                    ),
+                    TextFormField(
+                      textInputAction: TextInputAction.done,
+                      obscureText: true,
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return 'Por favor, informe a senha';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) => _password = value,
+                      style: TextStyle(fontSize: _getFontSize(context)),
+                      decoration: InputDecoration(
+                          contentPadding:
+                              EdgeInsets.only(left: 16.0, top: 16.0),
+                          labelText: 'Senha',
+                          labelStyle: TextStyle(fontSize: 20.0)),
+                    ),
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState?.validate() ?? false) {
+                            _formKey.currentState!.save();
+                            print('Login: $_username\nSenha: $_password');
+                            // Implementar a lógica de login aqui
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      QRPage(login: _username!)),
+                            );
+                          }
+                        },
+                        child: Text('Entrar',
+                            style: TextStyle(fontSize: _getFontSize(context))),
+                        style: ButtonStyle(
+                          minimumSize: MaterialStateProperty.all(Size(200, 50)),
+                        )),
+                  ],
                 ),
               ),
-              TextFormField(
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Por favor, informe o login';
-                  }
-                  return null;
-                },
-                textInputAction: TextInputAction.next,
-                onSaved: (value) => _username = value,
-                style: TextStyle(fontSize: _getFontSize(context)),
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 16.0),
-                    labelText: 'Login',
-                    labelStyle: TextStyle(fontSize: 20.0)),
-              ),
-              TextFormField(
-                textInputAction: TextInputAction.done,
-                obscureText: true,
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Por favor, informe a senha';
-                  }
-                  return null;
-                },
-                onSaved: (value) => _password = value,
-                style: TextStyle(fontSize: _getFontSize(context)),
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 16.0, top: 16.0),
-                    labelText: 'Senha',
-                    labelStyle: TextStyle(fontSize: 20.0)),
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState?.validate() ?? false) {
-                      _formKey.currentState!.save();
-                      print('Login: $_username\nSenha: $_password');
-                      // Implementar a lógica de login aqui
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => QRPage(login: _username!)),
-                      );
-                    }
-                  },
-                  child: Text('Entrar',
-                      style: TextStyle(fontSize: _getFontSize(context))),
-                  style: ButtonStyle(
-                    minimumSize: MaterialStateProperty.all(Size(200, 50)),
-                  )),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
